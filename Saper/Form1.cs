cb22db85c1091ea87df82edacee23ad1a10b5e7a
Fedor Radionov <Fed200522805@gmail.com>
2023-12-12 16:17:09 +0500

    Добавьте файлы проекта.

---
+++ Saper/Form1.cs
@@ -0,0 +1,24 @@
+﻿using Saper;
+using Saper.mapcontroller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MapController.Init(this);
+        }
+    }
+}
